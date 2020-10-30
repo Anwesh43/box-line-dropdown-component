eee4080305ebc5ac196f611aafe33f0f00674e1a
Anwesh43 <anweshthecool0@gmail.com>
2020-10-30 22:53:28 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from 'react'
-
+import { useState, useEffect } from 'react'
+import { sinify, divideScale } from './ho'
 
 export const useAnimatedScale = (scGap = 0.02, delay = 20) => {
     const [animated, setAnimated] = useState(false)
@@ -36,5 +36,47 @@ export const useDimension = () => {
     return {
         w, 
         h, 
+    }
+}
+
+
+export const useStyle = (w, h, scale) => {
+    const position = 'absolute'
+    const size = w / 9
+    const x = w / 2 -  size / 2 
+    const y = h / 5 
+    const sf = sinify(scale)
+    const sf1 = divideScale(sf, 0, 2)
+    const sf2 = divideScale(sf, 1, 2)
+    const background = 'green'
+    return {
+        blockStyle() {
+            const left = `${x + size * sf1}px`
+            const top = `${y}px`
+            const width = `${size / 10}px`
+            const height = `${size / 10}px`
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height, 
+                background
+            }
+        },
+        dropBoxStyle() {
+            const left = `${x}px`
+            const top = `${y + size / 10}px`
+            const width = `${size}px`
+            const height = `${size * sf2}px`
+            return {
+                position, 
+                left,
+                top, 
+                width, 
+                height, 
+                background
+            }
+        }
     }
 }
